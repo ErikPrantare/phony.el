254a7a4df8d550a3c8ed809f2cdbf815756ade41
Erik Präntare <erik@prantare.xyz>
2026-06-26 01:35:58 +0200

    Add creation date for parser

--- phony-parser.el
+++ phony-parser.el
@@ -3,6 +3,7 @@
 ;; Copyright (C) 2026 Erik Präntare
 
 ;; Author: Erik Präntare
+;; Created: 24 Jun 2026
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU Affero General Public License
